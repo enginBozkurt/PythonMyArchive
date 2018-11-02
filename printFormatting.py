73072a00e7492afe5713e6d29bab7ec1a69f7087
Engin Bozkurt <30608533+enginBozkurt@users.noreply.github.com>
2018-11-03 02:56:00 +0300

    Print Formatting

---
+++ printFormatting.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 17:39:48 2018
+
+@author: Engin Bozkurt
+"""
+
+# Print Formatting
+"First argument: {0}, second one: {1}".format(47,11)
+
+"Art: {a:5d},  Price: {p:8.2f}".format(a=453, p=59.05865)
+
+# Multiple times:
+"One: {p}, Two: {p}, Three: {p}".format(p='Hi!')
+
+# Several Objects:
+"Object 1: {a}, Object 2: {b}, Object 3: {c}".format(a=1,b='two',c=12.3)
